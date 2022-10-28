0607d5f42bb88eeabfc702b97372d83cbcbaf1cf
konstantinov-dv <konstantinov.dima.v@gmail.com>
2022-10-28 11:34:25 +0300

    Переменные. Часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        var d = 3;
+        // Переменные. Часть 2
+
+        // задача 1
+        int a = 1000000;
+        System.out.println(a);
+        byte b = 100;
+        System.out.println(b);
+        short c = 10000;
+        System.out.println(c);
+        long d = 15000000;
         System.out.println(d);
+        float i = 1.5f;
+        System.out.println(i);
+        double f = 25.5;
+        System.out.println(f);
+
+        // задача 2
+        double ad = 27.12;
+        System.out.println(ad);
+        long apple = 987678965549L;
+        System.out.println(apple);
+        double box = 2.786;
+        System.out.println(box);
+        boolean af = false;
+        System.out.println(af);
+        short ag = 569;
+        System.out.println(ag);
+        short ah = -159;
+        System.out.println(ah);
+        short aj = 27897;
+        System.out.println(aj);
+        byte ak = 67;
+        System.out.println(ak);
+
+        // задача 3
+
+
+
+
+
     }
 }
